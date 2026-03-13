d4a7eaa969dac4c995c9f6ebdc8ef73b75c496c0
saraldulal <saraldulal060@gmail.com>
2026-03-13 10:54:37 +0545

    wertyui

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -17,8 +17,8 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: 'saraldulal060@gmail.com',
+        pass: 'uxhz bcmf mfsy ropb',
       },
     });
 
